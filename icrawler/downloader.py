f87a79ad7a1bee89cfcd0b320643e161b64e357e
drimpossible <drimpossible@mailfence.com>
2023-07-24 20:56:55 +0100

    nice dataset structure

--- icrawler/downloader.py
+++ icrawler/downloader.py
@@ -293,7 +293,8 @@ class TextFileDownloader(Downloader):
         for worker in self.workers:
             worker.start()
             self.logger.debug('thread %s started', worker.name)
-        self.fwr = open(kwargs['query']+'_'+kwargs['engine']+'.txt','w')
+        os.makedirs(kwargs['savepath']+'/'+kwargs['engine']+'/'+kwargs['query']+'/images.txt', exist_ok=True)
+        self.fwr = open(kwargs['savepath']+'/'+kwargs['engine']+'/'+kwargs['query']+'/images.txt','w')
     
     def worker_exec(self, max_num, default_ext='jpg', queue_timeout=5, req_timeout=5, **kwargs):
         self.max_num = max_num
