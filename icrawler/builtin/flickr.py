880dfaf30c953269e6c7c76945c63b362fb88227
drimpossible <drimpossible@mailfence.com>
2023-07-31 10:34:39 +0100

    expanding images

--- icrawler/builtin/flickr.py
+++ icrawler/builtin/flickr.py
@@ -12,9 +12,9 @@ from icrawler import Crawler, Feeder, Parser, ImageDownloader
 
 class FlickrFeeder(Feeder):
 
-    def feed(self, apikey, max_num=250, **kwargs):
-        if max_num > 250:
-            max_num = 250
+    def feed(self, apikey, max_num=750, **kwargs):
+        if max_num > 750:
+            max_num = 750
             self.logger.warning(
                 'max_num exceeds 200, set it to 200 automatically.')
         base_url = 'https://api.flickr.com/services/rest/?'
@@ -48,7 +48,7 @@ class FlickrFeeder(Feeder):
         url = base_url + urlencode(params)
         per_page = params.get('per_page', 100)
         page = params.get('page', 1)
-        page_max = int(math.ceil(250.0 / per_page))
+        page_max = int(math.ceil(750.0 / per_page))
         for i in range(page, page + page_max):
             if self.signal.get('reach_max_num'):
                 break
